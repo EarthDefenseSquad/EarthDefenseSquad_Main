e85c708f956edb4f81aaf0b357b2872d4664eb31
Jsy00 <jsy00@sookmyung.ac.kr>
2025-07-29 17:20:41 +0900

    #fix : 보스전 매커니즘 수정 전

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,6 +21,8 @@ public class GameManager : MonoBehaviour
     public bool isBossActive = false; // 보스 활성화 여부
 
 
+
+
     // 현재까지 먹은 Finish 아이템 개수 (게임 재시작 시에도 유지됨)
     public int finishItemCount = 0;
 
