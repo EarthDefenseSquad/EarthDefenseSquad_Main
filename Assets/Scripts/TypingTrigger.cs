8ac7f1a43971dedece8d52631a55558cbea56efc
hy3m1n <hyem1210@sookmyung.ac.kr>
2025-08-12 10:19:53 +0900

    # 1111
    뭔지기억이

---
+++ Assets/Scripts/TypingTrigger.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class TypingTrigger : MonoBehaviour
+{
+    public string keyword;
+    private bool isUsed = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isUsed) return;
+
+        if (other.CompareTag("Player"))
+        {
+            TypingManager manager = FindObjectOfType<TypingManager>();
+            if (manager != null)
+            {
+                manager.ActivateTyping(keyword, gameObject);
+                isUsed = true;
+            }
+        }
+    }
+}
